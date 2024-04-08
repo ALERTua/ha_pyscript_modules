438322f5a1a22cec126e54953d97f921bc9048ac
Alexey ALERT Rubashёff <alexey.rubasheff@gmail.com>
2024-04-08 19:49:07 +0300

    + device entity

---
+++ entities/device.py
@@ -0,0 +1,24 @@
+from imports_base import *
+from homeassistant.helpers import device_registry
+
+
+class Device:
+    def __init__(self, device_id):
+        self.device_id = device_id
+
+    @pyscript_compile
+    def __repr__(self):
+        return f"Device: {self.device_id}"
+
+    def ha_device(self) -> device_registry.DeviceEntry:
+        registry = device_registry.async_get(hass)
+        return registry.async_get(self.device_id)
+
+    def disable(self):
+        # homeassistant.disable_device(device_id=self.device_id)  # https://spook.boo
+        device_registry.async_update_device(device_id=self.device_id,
+                                            disabled_by=device_registry.DeviceEntryDisabler.USER)
+
+    def enable(self):  # https://spook.boo
+        # homeassistant.enable_device(device_id=self.device_id)  # https://spook.boo
+        device_registry.async_update_device(device_id=self.device_id, disabled_by=None)
