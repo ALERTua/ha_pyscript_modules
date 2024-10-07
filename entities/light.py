c0fd5aea46cac6d2c350e106f4c1baeb4dfde527
Alexey ALERT Rubashёff <alexey.rubasheff@gmail.com>
2024-10-07 19:56:00 +0300

    ! iteration

--- entities/light.py
+++ entities/light.py
@@ -1,4 +1,5 @@
 # https://github.com/custom-components/pyscript
+from homeassistant.util.color import brightness_to_value
 from entities.switch import Switch
 
 
@@ -7,3 +8,8 @@ class Light(Switch):
     def __init__(self, entity_id):
         self.entity_id = entity_id
         self.init()
+
+    def brightness_percent(self):
+        brightness_value = self.state('brightness', 0)
+        brightness_value = brightness_value or 0
+        return int(round(brightness_to_value((1, 100), int(brightness_value)), 0))
--- scenarios/auto_boiler_control.py
+++ scenarios/auto_boiler_control.py
@@ -7,18 +7,23 @@ DISCORD_CHATS = ['1262407979920261161']
 
 def auto_boiler_control(trigger_type=None, var_name=None, value=None, old_value=None, context=None, **kwargs):
     dbg = False
-    ib_eid = kwargs.get('IB_EID', None)
     discount_period_eid = kwargs.get('DISCOUNT_PERIOD_EID', None)
     boiler_eid = kwargs.get('BOILER_EID', None)
     ignore_boiler_off = kwargs.get('IGNORE_BOILER_OFF', False)
-    discount_on_mode = kwargs.get('DISCOUNT_ON_MODE', 'eco')
-    discount_off_mode = kwargs.get('DISCOUNT_OFF_MODE', 'eco')
+    discount_on_mode = kwargs.get('MODE_DISCOUNT', 'performance')
+    discount_off_mode = kwargs.get('MODE', 'eco')
+    target_temperature_eid = kwargs.get('TARGET_TEMPERATURE_EID', 'number.boiler_target_temperature')
+    target_temperature_discount = kwargs.get('TARGET_TEMPERATURE_DISCOUNT', 65)
+    target_temperature = kwargs.get('TARGET_TEMPERATURE', 55)
+    showers_eid = kwargs.get('SHOWERS_EID', 'number.boiler_expected_number_of_shower')
+    showers_discount = kwargs.get('SHOWERS_DISCOUNT', 5)
+    showers = kwargs.get('SHOWERS', 4)
 
-    if any([_ for _ in (ib_eid, discount_period_eid, boiler_eid) if _ is None]):
+
+    if any([_ for _ in (discount_period_eid, boiler_eid) if _ is None]):
         log.error(f"{__name__} not all args are set:\n{pformat(locals())}")
         return
 
-    ib_e = entity(ib_eid)
     discount_period_e = entity(discount_period_eid)
     boiler_e = WaterHeater(boiler_eid)
     boiler_mode_current = boiler_e.state()
@@ -29,23 +34,46 @@ def auto_boiler_control(trigger_type=None, var_name=None, value=None, old_value=
             log.debug(f"{__name__}: boiler is off and ignore_boiler_off is not set. Not doing anything.")
         return
 
+    target_temperature_e = entity(target_temperature_eid)
+    target_temperature_current = float_(target_temperature_e.state())
+    showers_e = entity(showers_eid)
+    showers_current = int_(showers_e.state())
+
     e_checks = (
-        (ib_eid, ib_e),
         (discount_period_eid, discount_period_e),
         (boiler_eid, boiler_e),
+        (target_temperature_eid, target_temperature_e),
+        (showers_eid, showers_e),
     )
     for eid, e in e_checks:
         if e is None:
-            log.error(f"{__name__}: entity not found: {eid}")
+            msg = f"{__name__}: entity not found: {eid}"
+            log.error(msg)
+            tools.telegram_message_alert_ha_private(msg)
             return
 
     discount_period_on = discount_period_e.is_on()
     boiler_mode_needed = discount_on_mode if discount_period_on else discount_off_mode
-    if boiler_mode_current == boiler_mode_needed:
-        msg = f"{__name__}: discount_period: {discount_period_on}. boiler_mode_needed: {boiler_mode_needed} == {boiler_mode_current}. Nothing to do."
-    else:
-        msg = f"{__name__}: discount_period: {discount_period_on}. boiler_mode_needed: {boiler_mode_needed} vs {boiler_mode_current}. Setting mode to {boiler_mode_needed}."
-        boiler_e.set_operation_mode(boiler_mode_needed)
+    showers_needed = showers_discount if discount_period_on else showers
+    temperature_needed = target_temperature_discount if discount_period_on else target_temperature
 
-    log.debug(msg)
-    tools.discord_message(msg, target=DISCORD_CHATS)
+    msg = f'{__name__}:\ndiscount_period: {discount_period_on}'
+    action_taken = False
+    if boiler_mode_current != boiler_mode_needed:
+        msg += f"\nboiler_mode_needed: {boiler_mode_needed} vs {boiler_mode_current}.\nSetting mode to {boiler_mode_needed}."
+        boiler_e.set_operation_mode(boiler_mode_needed)
+        action_taken = True
+
+    if showers_current != showers_needed:
+        msg += f"\nshowers_needed: {showers_needed} vs {showers_current}.\nSetting showers to {showers_needed}."
+        showers_e.set_value(showers_needed)
+        action_taken = True
+
+    if target_temperature_current != temperature_needed:
+        msg += f"\ntemperature_needed: {temperature_needed} vs {target_temperature_current}.\nSetting temperature to {temperature_needed}."
+        target_temperature_e.set_value(temperature_needed)
+        action_taken = True
+
+    if action_taken:
+        log.debug(msg)
+        tools.discord_message(msg, target=DISCORD_CHATS)
